27cdd16a6bae86108554ecb446bb07012ed35b30
hyojeong <khj444453@gmail.com>
2023-03-03 00:20:02 +0900

    tdd wordwrap

---
+++ src/test/java/tdd/wordwrap/WrapperTest.java
@@ -0,0 +1,38 @@
+package tdd.wordwrap;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class WrapperTest {
+    @Test
+    public void should_wrap() {
+        assertWraps(null, 1, "");
+        assertWraps("", 1, "");
+        assertWraps("x", 1, "x");
+        assertWraps("xx", 1, "x\nx");
+        assertWraps("xxx", 1, "x\nx\nx");
+        assertWraps("x x", 1, "x\nx");
+        assertWraps("x xx", 3, "x\nxx");
+        assertWraps("four score and seven years ago our fathers brought forth upon this continent", 7, "four\nscore\nand\nseven\nyears\nago our\nfathers\nbrought\nforth\nupon\nthis\ncontine\nnt");
+    }
+
+    private void assertWraps(String s, int width, String expected) {
+        assertThat(wrap(s, width), is(expected));
+    }
+
+    private String wrap(String s, int width) {
+        if(s == null)
+            return "";
+        if(s.length() <= width)
+            return s;
+        else {
+            int breakPoint = s.lastIndexOf(" ", width);
+            if(breakPoint == -1)
+                breakPoint = width;
+            return s.substring(0, breakPoint) + "\n" + wrap(s.substring(breakPoint).trim(), width);
+        }
+    }
+}
